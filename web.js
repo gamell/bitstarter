1911d0c7dd5e07761abba3887c33605da5e8ff76
Joan Gamell <gamell@gmail.com>
2013-07-14 19:42:16 +0800

    serving indes.html content

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	var resBuf = fs.readFileSync('index.html'); 
+  	response.send(resBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
